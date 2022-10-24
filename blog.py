d464d53829764bb3f2cecd164818ebf9c24d56c2
karo <karo200122@gmail.com>
2022-10-25 05:19:41 +0800

    o

--- blog.py
+++ blog.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         if request.values['send'] == '送出':
@@ -11,5 +11,11 @@ def index():
     return render_template('index.html', name="")
 
 
+@app.route("/introduce")
+def introduce():
+    return render_template("introduce.html")
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run()
+    #app.run(host='0.0.0.0', port='5000', debug=True)
