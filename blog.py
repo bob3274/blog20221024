d351436b21986eb76e2ef2960788df14bd4ac9f6
karo <karo200122@gmail.com>
2022-10-25 01:26:41 +0800

    n

--- blog.py
+++ blog.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route("/blog")
-def blog():
-    return render_template("blog.html")
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        if request.values['send'] == '送出':
+            return render_template('index.html', name=request.values['user'])
+    return render_template('index.html', name="")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port='5000', debug=True)
