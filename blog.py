a287f504f9e3302e6daa1a11874219725bde65d7
karo <karo200122@gmail.com>
2022-10-24 23:04:29 +0800

    test1

--- blog.py
+++ blog.py
@@ -1,10 +1,12 @@
-from blog import Flask
-from blog import render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
 
 app.run()
